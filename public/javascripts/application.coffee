9cce92334dcf442dcc60a39e1c56954a84347009
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2011-07-27 00:11:44 +0100

    first step towards coffee!

---
+++ public/javascripts/application.coffee
@@ -0,0 +1,242 @@
+# coffee -cb public/javascripts/application.coffee
+$ ->
+  window.app ||= {}
+
+  app.Todo = Backbone.Model.extend
+    EMPTY: 'Empty todo...'
+
+    url: ->
+      id = this.get('id')
+      app.list_path + (if id then "/items/#{id}" else '/items') + '.json'
+
+    initialize: ->
+      if (!this.get('shortdesc'))
+        this.set 'shortdesc': this.EMPTY
+
+    toggle: ->
+      this.save
+        isdone: !this.get('isdone')
+
+    clear: ->
+      this.destroy()
+      this.view.remove()
+
+  app.TodoList = Backbone.Collection.extend
+    model: app.Todo
+  
+    url: ->
+     app.list_path + '/items.json'
+
+    done: ->
+      this.filter (todo) ->
+        todo.get('isdone')
+
+    remaining: ->
+      this.filter (todo) ->
+        !todo.get('isdone')
+
+    comparator: (todo) ->
+      todo.get('id')
+
+    # This method is overridden to save us messing around
+    # with socket_id filtering.
+    add: (models, options) ->
+      if _.isArray(models)
+        for model in models
+          if model.id and !app.Todos.get(model.id)
+            this._add(model, options)
+      else
+        if models.id && !app.Todos.get(models.id)
+          this._add(models, options)
+      this
+
+  app.Todos = new app.TodoList
+
+  app.TodoView = Backbone.View.extend
+    tagName: 'li'
+  
+    template: _.template $('#item-template').html()
+
+    events:
+      'click .check':               'toggleDone'
+      'click span.todo-edit':       'edit'
+      'click span.todo-destroy':    'clear'
+      'keypress .todo-input':       'updateOnEnter'
+
+    initialize: ->
+      _.bindAll(this, 'render', 'close')
+      this.model.bind('remove', this.remove)
+      this.model.bind('change', this.render)
+      this.model.view = this
+
+    render: ->
+      model = this.model.toJSON()
+      $(this.el).html(this.template(model))
+      this.setContent()
+      this
+
+    setContent: ->
+      shortdesc = this.model.get('shortdesc')
+      this.$('.todo-content').text(shortdesc)
+      this.edit_input = this.$('.todo-input')
+      this.edit_input.bind('blur', this.close)
+      this.edit_input.val(shortdesc)
+
+    toggleDone: ->
+      this.model.toggle()
+
+    edit: ->
+      $(this.el).addClass('editing')
+      this.edit_input.focus()
+
+    close: ->
+      this.model.save
+        shortdesc: this.edit_input.val()
+      $(this.el).removeClass('editing')
+
+    updateOnEnter: (e) ->
+      if e.keyCode == 13
+        this.close()
+
+    remove: ->
+      $(this.el).remove()
+
+    clear: ->
+      this.model.clear()
+
+  app.AppView = Backbone.View.extend
+    el: $('#todoapp')
+  
+    statsTemplate: _.template $('#stats-template').html()
+
+    events:
+      'keypress #new-todo':      'createOnEnter'
+      'focus #new-todo':         'showTooltip'
+      'blur #new-todo':          'hideTooltip'
+      'click .todo-clear a':     'clearCompleted'
+      'click .title p input':    'selectShareUrl'
+      'dblclick .title p input': 'selectShareUrl'
+
+    initialize: ->
+      _.bindAll(this, 'addOne', 'removeOne', 'addAll', 'render', 'showTooltip', 'hideTooltip')
+
+      this.input = this.$('#new-todo')
+
+      this.$('.ui-tooltip-top').hide()
+
+      app.Todos.bind('add', this.addOne)
+      app.Todos.bind('remove', this.removeOne)
+      app.Todos.bind('refresh', this.addAll)
+      app.Todos.bind('all', this.render)
+
+      app.Todos.fetch()
+
+    render: ->
+      this.$('#todo-stats').html this.statsTemplate
+        total: app.Todos.length
+        done: app.Todos.done().length
+        remaining: app.Todos.remaining().length
+
+    addOne: (todo) ->
+      view = new app.TodoView(model: todo)
+      this.$('#todo-list').append(view.render().el)
+    
+    removeOne: (todo) ->
+      this.$("#todo-item-" + todo.id).parent('li').remove()
+
+    addAll: ->
+      app.Todos.each (todo) ->
+        view = new app.TodoView(model: todo)
+        this.$('#todo-list').prepend(view.render().el)
+
+    newAttributes: ->
+      shortdesc: this.input.val()
+      isdone: false
+
+    createOnEnter: (e) ->
+      if e.keyCode == 13
+        app.Todos.create(this.newAttributes())
+        this.input.val('Adding...').addClass('working')
+        _.delay (el) ->
+          if el.val() == 'Adding...'
+            el.val('').blur().removeClass('working')
+        , 1000, this.input
+      
+    clearCompleted: ->
+      _.each(app.Todos.done(), (todo) ->
+        todo.clear()
+      )
+      false
+
+    showTooltip: (e) ->
+      document.title = "Todos"
+
+      tooltip = this.$('.ui-tooltip-top')
+      self = this
+
+      if this.tooltipTimeout
+        clearTimeout(this.tooltipTimeout)
+
+      this.tooltipTimeout = _.delay ->
+        tooltip.fadeIn(300)
+        self.tooltipTimeout = _.delay(self.hideTooltip, 2400)
+      , 400
+    
+    hideTooltip: ->
+      tooltip = this.$('.ui-tooltip-top')
+      if this.tooltipTimeout
+        clearTimeout(this.tooltipTimeout)
+
+      tooltip.fadeOut(300)
+  
+    selectShareUrl: (e) ->
+      $(e.currentTarget).select()
+
+  window.AppInstance = new app.AppView
+
+  pusher = new Pusher('511a5abb7486107ce643')
+  channel = pusher.subscribe(window.app.list_channel)
+
+  app.TodosBackpusher = new Backpusher(channel, app.Todos)
+
+  app.TodosBackpusher.bind 'remote_create', (model) ->
+    title = document.title
+    matches = title.match(/\[(\d+) (\w+)\]/)
+
+    console.log(matches)
+
+    if matches && matches[2] == 'new'
+      count = parseInt(matches[1], 10)
+      document.title = "Todos [#{++count} new]"
+    else
+      document.title = 'Todos [1 new]'
+
+  app.TodosBackpusher.bind 'remote_update', (model) ->
+    title = document.title
+    matches = title.match(/\[(\d+) (\w+)\]/)
+
+    if matches && matches[2] == 'updated'
+      count = parseInt(matches[1], 10)
+      document.title = "Todos [#{++count} updated]"
+    else
+      document.title = 'Todos [1 updated]'
+
+  app.TodosBackpusher.bind 'remote_destroy', (model) ->
+    title = document.title
+    matches = title.match(/\[(\d+) (\w+)\]/)
+
+    if matches && matches[2] == 'removed'
+      count = parseInt(matches[1], 10)
+      document.title = "Todos [#{++count} removed]"
+    else
+      document.title = 'Todos [1 removed]'
+
+  window.onfocus = ->
+    setTimeout ->
+      document.title = 'Todos'
+    , 2000
+
+  document.onfocusin = ->
+    setTimeout ->
+      document.title = 'Todos'
+    , 2000
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,65 +1,55 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-;$(function(){
-  //Backbone.emulateJSON = true;
-
-  window.app = window.app || {};
-
+$(function() {
+  var channel, pusher;
+  window.app || (window.app = {});
   app.Todo = Backbone.Model.extend({
     EMPTY: 'Empty todo...',
-
     url: function() {
-      var id = this.get('id');
-      return app.list_path + (id ? '/items/' + id : '/items') + '.json'
+      var id;
+      id = this.get('id');
+      return app.list_path + (id ? "/items/" + id : '/items') + '.json';
     },
-
     initialize: function() {
       if (!this.get('shortdesc')) {
-        this.set({'shortdesc': this.EMPTY});
+        return this.set({
+          'shortdesc': this.EMPTY
+        });
       }
     },
-
     toggle: function() {
-      this.save({
+      return this.save({
         isdone: !this.get('isdone')
       });
     },
-
     clear: function() {
       this.destroy();
-      this.view.remove();
+      return this.view.remove();
     }
   });
-
   app.TodoList = Backbone.Collection.extend({
     model: app.Todo,
     url: function() {
       return app.list_path + '/items.json';
     },
-
     done: function() {
       return this.filter(function(todo) {
         return todo.get('isdone');
       });
     },
-
     remaining: function() {
       return this.filter(function(todo) {
         return !todo.get('isdone');
       });
     },
-
     comparator: function(todo) {
       return todo.get('id');
     },
-
-    // This method is overridden to save us messing around
-    // with socket_id filtering.
     add: function(models, options) {
+      var model, _i, _len;
       if (_.isArray(models)) {
-        for (var i = 0, l = models.length; i < l; i++) {
-          if (models[i].id && !app.Todos.get(models[i].id)) {
-            this._add(models[i], options);
+        for (_i = 0, _len = models.length; _i < _len; _i++) {
+          model = models[_i];
+          if (model.id && !app.Todos.get(model.id)) {
+            this._add(model, options);
           }
         }
       } else {
@@ -70,231 +60,203 @@
       return this;
     }
   });
-
   app.Todos = new app.TodoList;
-
   app.TodoView = Backbone.View.extend({
     tagName: 'li',
-
     template: _.template($('#item-template').html()),
-
     events: {
-      'click .check':               'toggleDone',
-      'click span.todo-edit':       'edit',
-      'click span.todo-destroy':    'clear',
-      'keypress .todo-input':       'updateOnEnter'
+      'click .check': 'toggleDone',
+      'click span.todo-edit': 'edit',
+      'click span.todo-destroy': 'clear',
+      'keypress .todo-input': 'updateOnEnter'
     },
-
     initialize: function() {
       _.bindAll(this, 'render', 'close');
       this.model.bind('remove', this.remove);
       this.model.bind('change', this.render);
-      this.model.view = this;
+      return this.model.view = this;
     },
-
     render: function() {
-      var model = this.model.toJSON();
+      var model;
+      model = this.model.toJSON();
       $(this.el).html(this.template(model));
       this.setContent();
       return this;
     },
-
     setContent: function() {
-      var shortdesc = this.model.get('shortdesc');
+      var shortdesc;
+      shortdesc = this.model.get('shortdesc');
       this.$('.todo-content').text(shortdesc);
       this.edit_input = this.$('.todo-input');
       this.edit_input.bind('blur', this.close);
-      this.edit_input.val(shortdesc);
+      return this.edit_input.val(shortdesc);
     },
-
     toggleDone: function() {
-      this.model.toggle();
+      return this.model.toggle();
     },
-
     edit: function() {
       $(this.el).addClass('editing');
-      this.edit_input.focus();
+      return this.edit_input.focus();
     },
-
     close: function() {
       this.model.save({
-        shortdesc: this.edit_input.val(),
+        shortdesc: this.edit_input.val()
       });
-      $(this.el).removeClass('editing');
+      return $(this.el).removeClass('editing');
     },
-
     updateOnEnter: function(e) {
-      if(e.keyCode == 13) this.close();
+      if (e.keyCode === 13) {
+        return this.close();
+      }
     },
-
     remove: function() {
-      $(this.el).remove();
+      return $(this.el).remove();
     },
-
     clear: function() {
-      this.model.clear();
+      return this.model.clear();
     }
   });
-
   app.AppView = Backbone.View.extend({
     el: $('#todoapp'),
-
     statsTemplate: _.template($('#stats-template').html()),
-
     events: {
       'keypress #new-todo': 'createOnEnter',
-      'focus #new-todo':    'showTooltip',
-      'blur #new-todo':    'hideTooltip',
+      'focus #new-todo': 'showTooltip',
+      'blur #new-todo': 'hideTooltip',
       'click .todo-clear a': 'clearCompleted',
       'click .title p input': 'selectShareUrl',
       'dblclick .title p input': 'selectShareUrl'
     },
-
     initialize: function() {
       _.bindAll(this, 'addOne', 'removeOne', 'addAll', 'render', 'showTooltip', 'hideTooltip');
-
       this.input = this.$('#new-todo');
-
       this.$('.ui-tooltip-top').hide();
-
       app.Todos.bind('add', this.addOne);
       app.Todos.bind('remove', this.removeOne);
       app.Todos.bind('refresh', this.addAll);
       app.Todos.bind('all', this.render);
-
-      app.Todos.fetch();
+      return app.Todos.fetch();
     },
-
     render: function() {
-      this.$('#todo-stats').html(this.statsTemplate({
+      return this.$('#todo-stats').html(this.statsTemplate({
         total: app.Todos.length,
         done: app.Todos.done().length,
         remaining: app.Todos.remaining().length
       }));
     },
-
     addOne: function(todo) {
-      var view = new app.TodoView({model: todo});
-      this.$('#todo-list').append(view.render().el);
+      var view;
+      view = new app.TodoView({
+        model: todo
+      });
+      return this.$('#todo-list').append(view.render().el);
     },
-
     removeOne: function(todo) {
-      this.$("#todo-item-" + todo.id).parent('li').remove();
+      return this.$("#todo-item-" + todo.id).parent('li').remove();
     },
-
     addAll: function() {
-      app.Todos.each(function(todo) {
-        var view = new app.TodoView({model: todo});
-        this.$('#todo-list').prepend(view.render().el);
+      return app.Todos.each(function(todo) {
+        var view;
+        view = new app.TodoView({
+          model: todo
+        });
+        return this.$('#todo-list').prepend(view.render().el);
       });
     },
-
     newAttributes: function() {
       return {
         shortdesc: this.input.val(),
         isdone: false
       };
     },
-
     createOnEnter: function(e) {
-      if (e.keyCode != 13) return;
-
-      app.Todos.create(this.newAttributes());
-      this.input.val('Adding...').addClass('working');
-      _.delay(function(el) {
-        if (el.val() == 'Adding...') el.val('').blur().removeClass('working');
-      }, 1000, this.input);
+      if (e.keyCode === 13) {
+        app.Todos.create(this.newAttributes());
+        this.input.val('Adding...').addClass('working');
+        return _.delay(function(el) {
+          if (el.val() === 'Adding...') {
+            return el.val('').blur().removeClass('working');
+          }
+        }, 1000, this.input);
+      }
     },
-
     clearCompleted: function() {
       _.each(app.Todos.done(), function(todo) {
-        todo.clear();
+        return todo.clear();
       });
-
       return false;
     },
-
     showTooltip: function(e) {
+      var self, tooltip;
       document.title = "Todos";
-
-      var tooltip = this.$('.ui-tooltip-top');
-      var self = this;
-
-      if (this.tooltipTimeout) clearTimeout(this.tooltipTimeout);
-
-      this.tooltipTimeout = _.delay(function() {
+      tooltip = this.$('.ui-tooltip-top');
+      self = this;
+      if (this.tooltipTimeout) {
+        clearTimeout(this.tooltipTimeout);
+      }
+      return this.tooltipTimeout = _.delay(function() {
         tooltip.fadeIn(300);
-        self.tooltipTimeout = _.delay(self.hideTooltip, 2400);
+        return self.tooltipTimeout = _.delay(self.hideTooltip, 2400);
       }, 400);
     },
-
     hideTooltip: function() {
-      var tooltip = this.$('.ui-tooltip-top');
-      if (this.tooltipTimeout) clearTimeout(this.tooltipTimeout);
-
-      tooltip.fadeOut(300);
+      var tooltip;
+      tooltip = this.$('.ui-tooltip-top');
+      if (this.tooltipTimeout) {
+        clearTimeout(this.tooltipTimeout);
+      }
+      return tooltip.fadeOut(300);
     },
-
     selectShareUrl: function(e) {
-      $(e.currentTarget).select();
+      return $(e.currentTarget).select();
     }
   });
-
   window.AppInstance = new app.AppView;
-
-  var pusher = new Pusher('511a5abb7486107ce643');
-  var channel = pusher.subscribe(window.app.list_channel);
-
+  pusher = new Pusher('511a5abb7486107ce643');
+  channel = pusher.subscribe(window.app.list_channel);
   app.TodosBackpusher = new Backpusher(channel, app.Todos);
-
-
   app.TodosBackpusher.bind('remote_create', function(model) {
-    var title = document.title;
-    var matches = title.match(/\[(\d+) (\w+)\]/);
-
+    var count, matches, title;
+    title = document.title;
+    matches = title.match(/\[(\d+) (\w+)\]/);
     console.log(matches);
-
-    if (matches && matches[2] == 'new') {
-      var count = parseInt(matches[1], 10);
-      document.title = 'Todos [' + (++count) + ' new]';
+    if (matches && matches[2] === 'new') {
+      count = parseInt(matches[1], 10);
+      return document.title = "Todos [" + (++count) + " new]";
     } else {
-      document.title = 'Todos [1 new]';
+      return document.title = 'Todos [1 new]';
     }
   });
-
   app.TodosBackpusher.bind('remote_update', function(model) {
-    var title = document.title;
-    var matches = title.match(/\[(\d+) (\w+)\]/);
-
-    if (matches && matches[2] == 'updated') {
-      var count = parseInt(matches[1], 10);
-      document.title = 'Todos [' + (++count) + ' updated]';
+    var count, matches, title;
+    title = document.title;
+    matches = title.match(/\[(\d+) (\w+)\]/);
+    if (matches && matches[2] === 'updated') {
+      count = parseInt(matches[1], 10);
+      return document.title = "Todos [" + (++count) + " updated]";
     } else {
-      document.title = 'Todos [1 updated]';
+      return document.title = 'Todos [1 updated]';
     }
   });
-
   app.TodosBackpusher.bind('remote_destroy', function(model) {
-    var title = document.title;
-    var matches = title.match(/\[(\d+) (\w+)\]/);
-
-    if (matches && matches[2] == 'removed') {
-      var count = parseInt(matches[1], 10);
-      document.title = 'Todos [' + (++count) + ' removed]';
+    var count, matches, title;
+    title = document.title;
+    matches = title.match(/\[(\d+) (\w+)\]/);
+    if (matches && matches[2] === 'removed') {
+      count = parseInt(matches[1], 10);
+      return document.title = "Todos [" + (++count) + " removed]";
     } else {
-      document.title = 'Todos [1 removed]';
+      return document.title = 'Todos [1 removed]';
     }
   });
-
   window.onfocus = function() {
-    setTimeout(function(){
-      document.title = 'Todos';
+    return setTimeout(function() {
+      return document.title = 'Todos';
     }, 2000);
   };
-
-  document.onfocusin = function() {
-    setTimeout(function(){
-      document.title = 'Todos';
+  return document.onfocusin = function() {
+    return setTimeout(function() {
+      return document.title = 'Todos';
     }, 2000);
-  }
+  };
 });
